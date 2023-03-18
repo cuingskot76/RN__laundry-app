81fb1efc506d5c04a3c118fe4f045210adcdf29c
cuingskot76 <afrizapamujo@gmail.com>
2023-03-18 23:45:29 +0700

    add react native navigation

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>App</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
